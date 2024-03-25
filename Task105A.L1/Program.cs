dc3815ef0043e959ad985b4f02b0aa2061f5c51f
LamiyaAlikhanova <you@example.com>
2024-03-25 16:20:17 +0400

    class task

---
+++ Task105A.L1/Program.cs
@@ -0,0 +1,80 @@
+﻿using System.ComponentModel.Design;
+
+namespace Task105A.L1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+            bool IsStock=true;
+
+            NoteBook notebook1 = new NoteBook("Asus",0, 2500,16);
+            NoteBook notebook2 = new NoteBook("Hp", 20, 2100, 8);
+
+            notebook1.Sale();
+            notebook2.ShowFullData();
+                
+        }
+    }
+
+    public class Product 
+    {
+        public string Name;
+        public string Description;
+        public int Count;
+        public bool IsStock;
+        public double Price;
+
+        public  Product(string name, int count, double price)
+        {
+            this.Name = name;
+            this.Count = count;
+            this.Price = price;
+        }
+
+
+        
+    }
+
+    public class NoteBook : Product
+    {
+        public byte ram;
+        public int storage;
+
+        public NoteBook(string name, int count, double price, byte ram ): base(name, count, price) 
+        { 
+            this.ram = ram;
+            
+        }
+
+        public void Sale()
+           
+        {
+            if (Count > 0)
+            {
+                Count--;
+                Console.WriteLine($"name:{Name},count:{Count},price:{Price},byte:{ram}");
+            }
+            else
+            {
+                Console.WriteLine("Stokda Mehsul Yoxdur");
+            }
+        }
+        public void ShowFullData()
+        {
+            if (Count > 0)
+            {
+                Count--;
+                Console.WriteLine($"name:{Name}, description:{Description}, count:{Count}, IsStock:{IsStock}, price:{Price},byte:{ram}");
+            }
+            else 
+            {
+                Console.WriteLine("Stokda Mehsul Yoxdur");
+            }
+        }
+        
+        
+
+        
+    }
+}
---
+++ Tassk105A.L/Program.cs
@@ -0,0 +1,61 @@
+﻿using System.Xml.Schema;
+
+namespace Tassk105A.L 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car
+            {
+                Brand = "BMW",
+                Model = "M8",
+                Mile = 128000,
+                DoorsNum = 4
+            };
+
+            MotorCycle motorCycle = new MotorCycle 
+            {
+                Brand = "Kawasaki",
+                Model = "Zx-10r",
+                Mile = 22100,
+                WheelNum = 2
+            };
+
+            car.ShowFullInfo();
+            motorCycle.ShowFullInfo();
+        }
+    }
+    //Task-1
+     public class Vehicle
+    {
+        public string Brand;
+        public string Model;
+        public double Mile;
+    }
+    public class Car : Vehicle
+    {
+        public int DoorsNum;
+
+        public void ShowFullInfo()
+        {
+            Console.WriteLine($"Brand:{Brand},Model:{Model},Mile:{Mile},DoorsNum:{DoorsNum}");
+
+        }
+    }
+    public class MotorCycle: Vehicle
+    {
+        public int WheelNum;
+
+        public void ShowFullInfo()
+        {
+            Console.WriteLine($"Brand:{Brand},Model:{Model},Mile:{Mile},WheelNum:{WheelNum}");
+        }
+    }
+
+   
+
+}    
+
+
+
